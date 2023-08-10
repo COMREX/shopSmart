a3ef60410eda20a1c82eb8b300688a5f5354c6a0
muavia haidri <muavia00842@gmail.com>
2023-08-10 02:48:03 -0700

    make side bar and input for desktop

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import "./App.css";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import GlobalStyles from "./gloabalStyles";
-
-import LoginSignupPage from "./pages/loginSignupPage";
-import CustomerContact from "./pages/customerContact";
-import HomePage from "./pages/homePage";
-import MonthlyInventory from "./pages/monthlyInvertoryPage";
-import GroceryListPage from "./pages/groceryListPage";
-import DiscountPage from "./pages/discountPage";
-import AddInventory from "./pages/addInventory";
-import ShoppingPage from "./pages/shoppingPage";
+import Sidebar from "./components/sidebar/sidebar";
+// import LoginSignupPage from "./pages/loginSignupPage";
+// import CustomerContact from "./pages/customerContact";
+// import HomePage from "./pages/homePage";
+// import MonthlyInventory from "./pages/monthlyInvertoryPage";
+// import GroceryListPage from "./pages/groceryListPage";
+// import DiscountPage from "./pages/discountPage";
+// import AddInventory from "./pages/addInventory";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes>
+      <Sidebar />
+      {/* <Routes>
         <Route path="/" Component={LoginSignupPage} />
         <Route path="/home" Component={HomePage} />
         <Route path="/monthly-inventory" Component={MonthlyInventory} />
         <Route path="/discount" Component={DiscountPage} />
         <Route path="/grocery-list" Component={GroceryListPage} />
         <Route path="/add-inventory" Component={AddInventory} />
-        {/* <Route path="/shopping-page" Component={ShoppingPage} /> */}
         <Route path="customer-services" Component={CustomerContact} />
-      </Routes>
+      </Routes> */}
     </ThemeProvider>
   );
 }
--- src/components/utils/search-input/searchInput.js
+++ src/components/utils/search-input/searchInput.js
@@ -1,21 +1,48 @@
 import {
-  Wrapper,
+  SearchInputWrapper,
   SearchInputStyled,
   SearchIconStyled,
 } from "./searchInput.styles";
-import Svg1 from "../../../Assets/svgs/search-icon.svg";
 
-const SearchInput = ({ placeholder, marginBottom, value, onChange }) => {
+const SearchInput = ({
+  height,
+  width,
+  placeholder,
+  marginBottom,
+  value,
+  onChange,
+  borderRadius,
+  backgroundColor,
+  boxShadow,
+}) => {
   return (
-    <Wrapper style={{ marginBottom }}>
-      <SearchIconStyled src={Svg1} alt="avg"></SearchIconStyled>
+    <SearchInputWrapper
+      borderRadius={borderRadius}
+      backgroundColor={backgroundColor}
+      style={{ marginBottom }}
+    >
+      <SearchIconStyled
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 21 21"
+        fill="none"
+      >
+        <path
+          d="M20.7123 18.1558L16.6235 14.067C16.4389 13.8824 16.1888 13.7799 15.9263 13.7799H15.2578C16.3897 12.3322 17.0623 10.5113 17.0623 8.53042C17.0623 3.81818 13.2441 0 8.53188 0C3.81965 0 0.00146484 3.81818 0.00146484 8.53042C0.00146484 13.2427 3.81965 17.0608 8.53188 17.0608C10.5127 17.0608 12.3337 16.3882 13.7814 15.2563V15.9248C13.7814 16.1873 13.8839 16.4375 14.0685 16.622L18.1573 20.7109C18.5428 21.0964 19.1662 21.0964 19.5476 20.7109L20.7082 19.5502C21.0937 19.1647 21.0937 18.5414 20.7123 18.1558ZM8.53188 13.7799C5.63236 13.7799 3.28239 11.434 3.28239 8.53042C3.28239 5.6309 5.62826 3.28093 8.53188 3.28093C11.4314 3.28093 13.7814 5.62679 13.7814 8.53042C13.7814 11.4299 11.4355 13.7799 8.53188 13.7799Z"
+          fill-opacity="0.82"
+        />
+      </SearchIconStyled>
       <SearchInputStyled
         type="text"
         onChange={onChange}
         value={value}
         placeholder={placeholder}
+        height={height}
+        width={width}
+        borderRadius={borderRadius}
+        backgroundColor={backgroundColor}
+        style={{ boxShadow: boxShadow || "none" }}
       />
-    </Wrapper>
+    </SearchInputWrapper>
   );
 };
 
